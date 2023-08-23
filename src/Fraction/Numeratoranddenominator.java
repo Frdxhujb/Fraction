e2eb363018c7d29280ef7eff95b8e48884af6669
Ksenia Gromova <lady_gerdi@mail.ru>
2023-08-23 21:26:38 +0300

    Initial commit

---
+++ src/Fraction/Numeratoranddenominator.java
@@ -0,0 +1,53 @@
+package Fraction;
+public class Numeratoranddenominator {
+    private int numerator;
+    private int denominator;
+
+    public Numeratoranddenominator(int numerator, int denominator) {
+        this.denominator=denominator;
+        this.numerator=numerator;
+    }
+    public Numeratoranddenominator plus (Numeratoranddenominator f1){
+        numerator+numerator==
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    @Override
+    public String toString() {
+        return "Numeratoranddenominator{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+}
+//class Math{
+//
+//
+//    static int q (int x, int y){
+//        return x * y/ v(x,y);
+//    }
+//    static int v(int x, int Y){
+//        if (x==0){
+//            return x;
+//        }
+//        return v(x,Y% x);
+//    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Fraction.Numeratoranddenominator;
+
+public class Main {
+    public static void main(String[] args) {
+        Numeratoranddenominator numeratoranddenominator = new Numeratoranddenominator(1, 3);
+
+
+
+    }
+}
